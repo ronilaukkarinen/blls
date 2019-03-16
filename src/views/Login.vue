0d52186ee700d3bc25e3c8d05e547263193bedf7
Roni Laukkarinen <roni@laukkarinen.info>
2019-03-16 13:59:47 +0200

    Save

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -10,58 +10,58 @@
                 <form v-if="showLoginForm" @submit.prevent>
                     <h1>Tervetuloa takaisin</h1>                    
 
-                    <label for="email1">Email</label>
-                    <input v-model.trim="loginForm.email" type="text" placeholder="you@email.com" id="email1" />
+                    <label for="email1">Sähköposti</label>
+                    <input v-model.trim="loginForm.email" type="text" placeholder="" id="email1" />
 
-                    <label for="password1">Password</label>
-                    <input v-model.trim="loginForm.password" type="password" placeholder="******" id="password1" />
+                    <label for="password1">Salasana</label>
+                    <input v-model.trim="loginForm.password" type="password" placeholder="" id="password1" />
 
-                    <button @click="login" class="button">Log In</button>
+                    <button @click="login" class="button">Kirjaudu sisään</button>
 
                     <div class="extras">
-                        <a @click="togglePasswordReset">Forgot Password</a>
-                        <a @click="toggleForm">Create an Account</a>
+                        <a @click="togglePasswordReset">Unohditko salasanasi?</a>
+                        <a @click="toggleForm">Rekisteröidy</a>
                     </div>
                 </form>
                 <form v-if="!showLoginForm && !showForgotPassword" @submit.prevent>
-                    <h1>Get Started</h1>
+                    <h1>Rekisteröityminen</h1>
 
-                    <label for="name">Name</label>
-                    <input v-model.trim="signupForm.name" type="text" placeholder="Savvy Apps" id="name" />
+                    <label for="name">Nimi</label>
+                    <input v-model.trim="signupForm.name" type="text" placeholder="" id="name" />
 
-                    <label for="title">Title</label>
-                    <input v-model.trim="signupForm.title" type="text" placeholder="Company" id="title" />
+                    <label for="title">Yritys</label>
+                    <input v-model.trim="signupForm.title" type="text" placeholder="" id="title" />
 
-                    <label for="email2">Email</label>
+                    <label for="email2">Sähköpostiosoite</label>
                     <input v-model.trim="signupForm.email" type="text" placeholder="you@email.com" id="email2" />
 
-                    <label for="password2">Password</label>
-                    <input v-model.trim="signupForm.password" type="password" placeholder="min 6 characters" id="password2" />
+                    <label for="password2">Salasana</label>
+                    <input v-model.trim="signupForm.password" type="password" placeholder="" id="password2" />
 
-                    <button @click="signup" class="button">Sign Up</button>
+                    <button @click="signup" class="button">Rekisteröidy</button>
 
                     <div class="extras">
-                        <a @click="toggleForm">Back to Log In</a>
+                        <a @click="toggleForm">Takaisin kirjautumiseen</a>
                     </div>
                 </form>
                 <form v-if="showForgotPassword" @submit.prevent class="password-reset">
                     <div v-if="!passwordResetSuccess">
-                        <h1>Reset Password</h1>
-                        <p>We will send you an email to reset your password</p>
+                        <h1>Nollaa salasanasi</h1>
+                        <p>Lähetämme sinulle sähköpostiin linkin, jonka avulla voit luoda uuden salasanan.</p>
 
-                        <label for="email3">Email</label>
+                        <label for="email3">Sähköpostiosoitteesi</label>
                         <input v-model.trim="passwordForm.email" type="text" placeholder="you@email.com" id="email3" />
 
-                        <button @click="resetPassword" class="button">Submit</button>
+                        <button @click="resetPassword" class="button">Lähetä</button>
 
                         <div class="extras">
-                            <a @click="togglePasswordReset">Back to Log In</a>
+                            <a @click="togglePasswordReset">Takaisin kirjautumiseen</a>
                         </div>
                     </div>
                     <div v-else>
-                        <h1>Email Sent</h1>
-                        <p>check your email for a link to reset your password</p>
-                        <button @click="togglePasswordReset" class="button">Back to login</button>
+                        <h1>Onnistui!</h1>
+                        <p>Katso sähköpostilaatikostasi ohjeet salasanan nollaamiseen.</p>
+                        <button @click="togglePasswordReset" class="button">Takaisin kirjautumiseen</button>
                     </div>
                 </form>
                 <transition name="fade">
