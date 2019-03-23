b2c314d669e2b35746cf44efb1d03da3990f8fd0
Roni Laukkarinen <roni@laukkarinen.info>
2019-03-23 19:17:42 +0200

    Error message styles

--- resources/views/partials/alerts/danger.blade.php
+++ resources/views/partials/alerts/danger.blade.php
@@ -1,11 +1,9 @@
 <div class="row {{ $payload['classes'] }}" style="
-    padding: 15px;
-    color: #FFF;
-    background: #F86380;
-    border-radius: 5px;
+padding: 15px;
+color: #fff;
+background: #f86380;
+border-radius: 5px;
+margin-bottom: 15px;
 ">
-    <div class="row__column row__column--compact">
-        <i class="fas fa-times"></i>
-    </div>
-    <div class="row__column text-center" style="font-weight: 600;">{{ $payload['message'] }}</div>
+<div class="message message-error"><svg width="16" height="16" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M1490 1322q0 40-28 68l-136 136q-28 28-68 28t-68-28l-294-294-294 294q-28 28-68 28t-68-28l-136-136q-28-28-28-68t28-68l294-294-294-294q-28-28-28-68t28-68l136-136q28-28 68-28t68 28l294 294 294-294q28-28 68-28t68 28l136 136q28 28 28 68t-28 68l-294 294 294 294q28 28 28 68z"/></svg>{{ $payload['message'] }}</div>
 </div>
