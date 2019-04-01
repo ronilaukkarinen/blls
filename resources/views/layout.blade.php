7a451417fd372ae4246483863cbe300743e3daf9
Roni Laukkarinen <roni@laukkarinen.info>
2019-04-01 22:25:26 +0300

    Fix default favicon

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,11 +7,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <link rel="apple-touch-icon" sizes="180x180" href="images/apple-touch-icon.png">
-  <link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png">
-  <link rel="icon" type="image/png" sizes="16x16" href="images/favicon-16x16.png">
   <link rel="manifest" href="images/site.webmanifest">
   <link rel="mask-icon" href="images/safari-pinned-tab.svg" color="#244e81">
-  <link rel="shortcut icon" href="images/favicon.ico">
+  <link rel="shortcut icon" href="images/favicon.png">
   <meta name="msapplication-TileColor" content="#2b5797">
   <meta name="msapplication-config" content="images/browserconfig.xml">
   <meta name="theme-color" content="#1d4d82">
