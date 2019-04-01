c31996f2bf325d6d945b7e07837d2798ebdb5a97
Roni Laukkarinen <roni@laukkarinen.info>
2019-04-01 22:21:26 +0300

    Favicons and app icons

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,8 +5,17 @@
   <meta name="csrf-token" content="{{ csrf_token() }}" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="apple-touch-icon" href="/images/apple-touch-icon.png">
-  <link rel="icon" href="/images/favicon.png">
+
+  <link rel="apple-touch-icon" sizes="180x180" href="images/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="images/favicon-16x16.png">
+  <link rel="manifest" href="images/site.webmanifest">
+  <link rel="mask-icon" href="images/safari-pinned-tab.svg" color="#244e81">
+  <link rel="shortcut icon" href="images/favicon.ico">
+  <meta name="msapplication-TileColor" content="#2b5797">
+  <meta name="msapplication-config" content="images/browserconfig.xml">
+  <meta name="theme-color" content="#1d4d82">
+
   <script src="/js/app.js"></script>
   <link rel="stylesheet" href="/css/app.css" />
 </head>
