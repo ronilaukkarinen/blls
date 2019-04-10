5f566a11e2621eda4c3023b7b867d87f76129eab
Roni Laukkarinen <roni@laukkarinen.info>
2019-04-10 09:33:30 +0300

    Remove double initial scale

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,7 +3,6 @@
 <head>
   <title>{{ View::hasSection('title') ? View::getSection('title') . ' - ' . config('app.name') : config('app.name') }}</title>
   <meta name="csrf-token" content="{{ csrf_token() }}" />
-  <meta name="viewport" content="width=device-width, initial-scale=1" />
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <link rel="apple-touch-icon" sizes="180x180" href="images/apple-touch-icon.png">
